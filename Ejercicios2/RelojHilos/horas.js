52cbba02ed1646cdc1d5e140ba01ba4b5d00703e
David Couñago González <david.counagogonzalez@daw2-12.dip.local>
2024-01-17 18:02:33 +0100

    Reloj digital con Workers

---
+++ Ejercicios2/RelojHilos/horas.js
@@ -0,0 +1,14 @@
+function horaActual(){
+    const fecha = new Date();
+    
+    
+    var horaCompleta = Object();
+    horaCompleta.hora = fecha.getUTCHours();
+    horaCompleta.minutos = fecha.getUTCMinutes();
+    horaCompleta.segundos = fecha.getUTCSeconds();
+
+    postMessage(horaCompleta);
+    setTimeout("horaActual()", 1000);
+}
+
+horaActual();
