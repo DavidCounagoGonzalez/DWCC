a1c407041fbe67ecfa71355f24b590400dfcd499
David Couñago González <david.counagogonzalez@daw2-12.dip.local>
2024-01-31 18:14:29 +0100

    Comentado

--- Ejercicios2/DWCC/NoticiasV3/getNoticias.js
+++ Ejercicios2/DWCC/NoticiasV3/getNoticias.js
@@ -1,10 +1,10 @@
+//Creamos un array vacío que serán nuestros datos de envío, el objeto xmlhttp y el json.
 noticias = [];
-
 xmlhttp = new XMLHttpRequest();
 url = "noticias.json";
 
 function getNoticias(noticia){
-
+    //Recorremos el json parseado y guardamos sus datos en el array; 
     for(i = 0; i < noticia.length; i++){
         registro = {};
 
@@ -16,6 +16,8 @@ function getNoticias(noticia){
 }
 
 function procesar(){
+    // Cuando el xmlhttp lanza el archivo dado en el url, si todo se ha salido bien parsea el json recibido en la lectura del xmlhttp, 
+    // lanza la función anterior y postea el resultado.
     xmlhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             noticiasJSON = JSON.parse(this.responseText);
@@ -25,7 +27,7 @@ function procesar(){
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-    setTimeout("procesar()", 1000);
+    setTimeout("procesar()", 1000); // La función se relanza cada X milisegundos
 }
 
 procesar();
